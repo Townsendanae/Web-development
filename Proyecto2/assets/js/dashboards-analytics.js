4d1f04ef923ff095100950d40338c557d0f5730f
Danae Townsend <danjtown@espol.edu.ec>
2022-07-17 11:37:37 -0500

    Agregado el donut char de especies. Notas para siguiente modificacion

--- Proyecto2/assets/js/dashboards-analytics.js
+++ Proyecto2/assets/js/dashboards-analytics.js
@@ -17,7 +17,7 @@
   let arrayIndices = [0];
   const btnCambiarPersonaje = document.getElementById('btnCambiarPersonaje');
   btnCambiarPersonaje.addEventListener('click', (event) => {
-    if (arrayIndices.length == 10){
+    if (arrayIndices.length == 11){
       arrayIndices = [0];
     }
     let newIndice;
@@ -34,7 +34,7 @@
     const contenedor = document.getElementById('ImagenCarrusel');
       contenedor.innerHTML = `<img
       src="../assets/img/personajes/${indice}.png"
-      height="140"
+      height="200"
       alt="View Badge User"
       data-app-dark-img="illustrations/man-with-laptop-dark.png"
       data-app-light-img="illustrations/man-with-laptop-light.png"
@@ -339,6 +339,7 @@
 
 
   //Card title -- card-title mb-2People
+  // --------------------------------------------------------------------
 
   let cargarTotalPersonas = () => {
     fetch("https://swapi.dev/api/people/?format=json")
@@ -350,7 +351,9 @@
       .catch(console.error);
   }
   cargarTotalPersonas()
+
   //Card title -- NavesTotales
+  // --------------------------------------------------------------------  
   let cargarTotalNaves = () => {
     fetch("https://swapi.dev/api/starships/?format=json")
       .then(response => response.json())
@@ -360,268 +363,7 @@
       })
       .catch(console.error);
   }
-
-
   cargarTotalNaves();
-  // const totalRevenueChartEl = document.querySelector('#totalRevenueChart'),
-  //   totalRevenueChartOptions = {
-  //     series: [
-  //       {
-  //         name: '2021',
-  //         data: [18, 7, 15, 29, 18, 12, 9]
-  //       },
-  //       {
-  //         name: '2020',
-  //         data: [13, -18, -9, -14, -5, -17, -15]
-  //       }
-  //     ],
-  //     chart: {
-  //       height: 300,
-  //       stacked: true,
-  //       type: 'bar',
-  //       toolbar: { show: false }
-  //     },
-  //     plotOptions: {
-  //       bar: {
-  //         horizontal: false,
-  //         columnWidth: '33%',
-  //         borderRadius: 12,
-  //         startingShape: 'rounded',
-  //         endingShape: 'rounded'
-  //       }
-  //     },
-  //     colors: [config.colors.primary, config.colors.info],
-  //     dataLabels: {
-  //       enabled: false
-  //     },
-  //     stroke: {
-  //       curve: 'smooth',
-  //       width: 6,
-  //       lineCap: 'round',
-  //       colors: [cardColor]
-  //     },
-  //     legend: {
-  //       show: true,
-  //       horizontalAlign: 'left',
-  //       position: 'top',
-  //       markers: {
-  //         height: 8,
-  //         width: 8,
-  //         radius: 12,
-  //         offsetX: -3
-  //       },
-  //       labels: {
-  //         colors: axisColor
-  //       },
-  //       itemMargin: {
-  //         horizontal: 10
-  //       }
-  //     },
-  //     grid: {
-  //       borderColor: borderColor,
-  //       padding: {
-  //         top: 0,
-  //         bottom: -8,
-  //         left: 20,
-  //         right: 20
-  //       }
-  //     },
-  //     xaxis: {
-  //       categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
-  //       labels: {
-  //         style: {
-  //           fontSize: '13px',
-  //           colors: axisColor
-  //         }
-  //       },
-  //       axisTicks: {
-  //         show: false
-  //       },
-  //       axisBorder: {
-  //         show: false
-  //       }
-  //     },
-  //     yaxis: {
-  //       labels: {
-  //         style: {
-  //           fontSize: '13px',
-  //           colors: axisColor
-  //         }
-  //       }
-  //     },
-  //     responsive: [
-  //       {
-  //         breakpoint: 1700,
-  //         options: {
-  //           plotOptions: {
-  //             bar: {
-  //               borderRadius: 10,
-  //               columnWidth: '32%'
-  //             }
-  //           }
-  //         }
-  //       },
-  //       {
-  //         breakpoint: 1580,
-  //         options: {
-  //           plotOptions: {
-  //             bar: {
-  //               borderRadius: 10,
-  //               columnWidth: '35%'
-  //             }
-  //           }
-  //         }
-  //       },
-  //       {
-  //         breakpoint: 1440,
-  //         options: {
-  //           plotOptions: {
-  //             bar: {
-  //               borderRadius: 10,
-  //               columnWidth: '42%'
-  //             }
-  //           }
-  //         }
-  //       },
-  //       {
-  //         breakpoint: 1300,
-  //         options: {
-  //           plotOptions: {
-  //             bar: {
-  //               borderRadius: 10,
-  //               columnWidth: '48%'
-  //             }
-  //           }
-  //         }
-  //       },
-  //       {
-  //         breakpoint: 1200,
-  //         options: {
-  //           plotOptions: {
-  //             bar: {
-  //               borderRadius: 10,
-  //               columnWidth: '40%'
-  //             }
-  //           }
-  //         }
-  //       },
-  //       {
-  //         breakpoint: 1040,
-  //         options: {
-  //           plotOptions: {
-  //             bar: {
-  //               borderRadius: 11,
-  //               columnWidth: '48%'
-  //             }
-  //           }
-  //         }
-  //       },
-  //       {
-  //         breakpoint: 991,
-  //         options: {
-  //           plotOptions: {
-  //             bar: {
-  //               borderRadius: 10,
-  //               columnWidth: '30%'
-  //             }
-  //           }
-  //         }
-  //       },
-  //       {
-  //         breakpoint: 840,
-  //         options: {
-  //           plotOptions: {
-  //             bar: {
-  //               borderRadius: 10,
-  //               columnWidth: '35%'
-  //             }
-  //           }
-  //         }
-  //       },
-  //       {
-  //         breakpoint: 768,
-  //         options: {
-  //           plotOptions: {
-  //             bar: {
-  //               borderRadius: 10,
-  //               columnWidth: '28%'
-  //             }
-  //           }
-  //         }
-  //       },
-  //       {
-  //         breakpoint: 640,
-  //         options: {
-  //           plotOptions: {
-  //             bar: {
-  //               borderRadius: 10,
-  //               columnWidth: '32%'
-  //             }
-  //           }
-  //         }
-  //       },
-  //       {
-  //         breakpoint: 576,
-  //         options: {
-  //           plotOptions: {
-  //             bar: {
-  //               borderRadius: 10,
-  //               columnWidth: '37%'
-  //             }
-  //           }
-  //         }
-  //       },
-  //       {
-  //         breakpoint: 480,
-  //         options: {
-  //           plotOptions: {
-  //             bar: {
-  //               borderRadius: 10,
-  //               columnWidth: '45%'
-  //             }
-  //           }
-  //         }
-  //       },
-  //       {
-  //         breakpoint: 420,
-  //         options: {
-  //           plotOptions: {
-  //             bar: {
-  //               borderRadius: 10,
-  //               columnWidth: '52%'
-  //             }
-  //           }
-  //         }
-  //       },
-  //       {
-  //         breakpoint: 380,
-  //         options: {
-  //           plotOptions: {
-  //             bar: {
-  //               borderRadius: 10,
-  //               columnWidth: '60%'
-  //             }
-  //           }
-  //         }
-  //       }
-  //     ],
-  //     states: {
-  //       hover: {
-  //         filter: {
-  //           type: 'none'
-  //         }
-  //       },
-  //       active: {
-  //         filter: {
-  //           type: 'none'
-  //         }
-  //       }
-  //     }
-  //   };
-  // if (typeof totalRevenueChartEl !== undefined && totalRevenueChartEl !== null) {
-  //   const totalRevenueChart = new ApexCharts(totalRevenueChartEl, totalRevenueChartOptions);
-  //   totalRevenueChart.render();
-  // }
 
 
   // Presentación naves
@@ -826,22 +568,52 @@
     profileReportChart.render();
   }
 
-  // Order Statistics Chart
+  // Distribución de especies en personajes principales
   // --------------------------------------------------------------------
-  const chartOrderStatistics = document.querySelector('#orderStatisticsChart'),
+
+
+  async function cargarDatosEspecies() {
+
+    let especiesArray = []
+    let cantidadPorEspecie = []
+
+    let siguiente = "https://swapi.dev/api/species/?format=json"
+
+    while (siguiente != null){
+      const response = await fetch(siguiente);
+      const data = await response.json();
+      siguiente = data['next'];
+
+      for (let datos of data['results']) {
+
+        let nombre = datos['name'];
+        especiesArray.push(nombre);
+
+        let cantidadPersonas = datos['people'].length
+        cantidadPorEspecie.push(cantidadPersonas)
+      }
+    }
+
+    //obtener quienes son los humanos, doides, gungan, Tholothian para presentar en el panel de abajo. 
+    // cambiar el total de ordenes a total de especies 
+    // pasar el char de plantilla de profile report a cantidad de planetas en el film 
+    //cambiar el char de profile report y tomar como plantilla el de especies y caracteristicas para el de profile de algunos planetas. 
+    //eliminar payments y tansactions
+    // si le da click a conocer cuales son: presentar un popup con la info de cuales son detallada. (expenses) eliminar profit
+    // en los 3 puntitos de cada recuadro, poner "Conoce quienes son " personajes importantes y naves 
+
+    const chartOrderStatistics = document.querySelector('#orderStatisticsChart'),
     orderChartConfig = {
       chart: {
-        height: 165,
-        width: 130,
         type: 'donut'
       },
-      labels: ['Electronic', 'Sports', 'Decor', 'Fashion'],
-      series: [85, 15, 50, 50],
-      colors: [config.colors.primary, config.colors.secondary, config.colors.info, config.colors.success],
-      stroke: {
-        width: 5,
-        colors: cardColor
-      },
+      labels: especiesArray,
+      series: cantidadPorEspecie,
+      // colors: [config.colors.primary, config.colors.secondary, config.colors.info, config.colors.success],
+      // stroke: {
+      //   width: 5,
+      //   colors: cardColor
+      // },
       dataLabels: {
         enabled: false,
         formatter: function (val, opt) {
@@ -896,6 +668,15 @@
     statisticsChart.render();
   }
 
+  }
+
+  cargarDatosEspecies()
+
+
+
+
+  
+
   // Cantidad de planetas que hay en cada film - Area chart
   // --------------------------------------------------------------------
 
@@ -942,103 +723,14 @@
     });
   }
 
+  fetch('https://swapi.dev/api/planets/?format=json')
+  .then(response => response.json())
+  .then(data => {
+    let total = data['count'];
+    document.getElementById("totalPlanetas").innerHTML = `${total}`;  
+  })
+  .catch(console.error);
   cargarDatos()
-  // const incomeChartEl = document.querySelector('#incomeChart'),
-  //     incomeChartConfig = {
-  //       series: [
-  //         {
-  //           data: [24, 21, 30, 22, 42, 26, 35, 29] /// cantidad de veces que se repite un planeta en todas las peliculas
-  //         }
-  //       ],
-  //       chart: {
-  //         height: 215,
-  //         parentHeightOffset: 0,
-  //         parentWidthOffset: 0,
-  //         toolbar: {
-  //           show: false
-  //         },
-  //         type: 'area'
-  //       },
-  //       dataLabels: {
-  //         enabled: false
-  //       },
-  //       stroke: {
-  //         width: 2,
-  //         curve: 'smooth'
-  //       },
-  //       legend: {
-  //         show: false
-  //       },
-  //       markers: {
-  //         size: 6,
-  //         colors: 'transparent',
-  //         strokeColors: 'transparent',
-  //         strokeWidth: 4,
-  //         discrete: [
-  //           {
-  //             fillColor: config.colors.white,
-  //             seriesIndex: 0,
-  //             dataPointIndex: 7,
-  //             strokeColor: config.colors.primary,
-  //             strokeWidth: 2,
-  //             size: 6,
-  //             radius: 8
-  //           }
-  //         ],
-  //         hover: {
-  //           size: 7
-  //         }
-  //       },
-  //       colors: [config.colors.primary],
-  //       fill: {
-  //         type: 'gradient',
-  //         gradient: {
-  //           shade: shadeColor,
-  //           shadeIntensity: 0.6,
-  //           opacityFrom: 0.5,
-  //           opacityTo: 0.25,
-  //           stops: [0, 95, 100]
-  //         }
-  //       },
-  //       grid: {
-  //         borderColor: borderColor,
-  //         strokeDashArray: 3,
-  //         padding: {
-  //           top: -20,
-  //           bottom: -8,
-  //           left: -10,
-  //           right: 8
-  //         }
-  //       },
-  //       xaxis: {
-  //         categories: ['', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
-  //         axisBorder: {
-  //           show: false
-  //         },
-  //         axisTicks: {
-  //           show: false
-  //         },
-  //         labels: {
-  //           show: true,
-  //           style: {
-  //             fontSize: '13px',
-  //             colors: axisColor
-  //           }
-  //         }
-  //       },
-  //       yaxis: {
-  //         labels: {
-  //           show: false
-  //         },
-  //         min: 10,
-  //         max: 50,
-  //         tickAmount: 4
-  //       }
-  //     };
-  //   if (typeof incomeChartEl !== undefined && incomeChartEl !== null) {
-  //     const incomeChart = new ApexCharts(incomeChartEl, incomeChartConfig);
-  //     incomeChart.render();
-  //   }
 
   // Expenses Mini Chart - Radial Chart
   // --------------------------------------------------------------------
@@ -1113,3 +805,7 @@
     weeklyExpenses.render();
   }
 })();
+
+  // Especies y características - List
+  // --------------------------------------------------------------------
+
